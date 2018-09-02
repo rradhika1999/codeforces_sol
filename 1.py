0366a07b6b873068749b23332e7b2e873c7ec2bd
gopavasanth <gopavasanth1999@gmail.com>
2018-09-02 10:29:10 +0530

    New File

---
+++ 1.py
@@ -0,0 +1,18 @@
+n=int(input())
+b=[]
+c=0
+sum=0
+for i in range (n):
+    b[i] = int(input())
+
+for j in range (n):
+    for k in range (n):
+        if b[j]==b[k]:
+            c++
+    if c>2:
+        l.append[]
+    
+for p in range (len(l)):
+    sum=sum+l[i]
+
+print (l)
