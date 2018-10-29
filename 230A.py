ef12013504383dac4e87157f7404eafe94f0ea49
Athi Dsh <32543864+Athi101@users.noreply.github.com>
2018-10-29 11:15:38 +0530

    Update 230A.py

--- 230A.py
+++ 230A.py
@@ -1,19 +1,18 @@
 m, n = [int(x) for x in input().split()]
+#n is the no of test cases, m is the initial strength
 l = []
 for i in range(n):
 	l.append([int(x) for x in input().split()])
 c=0
+
 l.sort()
-#print(l)
+
 if m>=l[0][0]:
     m = m + l[0][1]
 for i in range(n-1):
     if m >= l[i+1][0]:
         m=m+ l[i+1][1]
         c =1
-
-#print(c)        
-        
 if c==1:
     print("YES")
 else:
