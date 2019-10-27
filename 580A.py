623783ae0be0ee5b03fa286861299d3a903a368c
manira1 <35557678+manira1@users.noreply.github.com>
2019-10-27 16:06:37 +0530

    Update 580A.py

--- 580A.py
+++ 580A.py
@@ -4,13 +4,13 @@ count = 1
 large = 0
 b = []
 for i in range(len(l)-1):
-    if l[i] <= l[i+1]:
+    if (l[i] <= l[i+1]):
         count = count + 1
-        if count >=large:
+        if (count >=large):
             large = count
     else:
         count=1
-if n <= 1 or large == 0:
+if (n <= 1 or large == 0):
     print (1)
 else:
     print(large)  
