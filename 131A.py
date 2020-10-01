1c1b2c8afbd179e7de393ab756b090b28a8da057
Athi Dsh <32543864+Athi101@users.noreply.github.com>
2020-10-01 21:41:21 +0530

    Update 131A.py
    The code I've added just checks for the 2 conditions, it avoids unnecessary if.

--- 131A.py
+++ 131A.py
@@ -11,4 +11,12 @@ elif len(l) == 1:
          print (l.upper())
 else:
     print (l)
-        
+    
+#instead of many if-else
+a=str(input())
+if ((a[1:]==a[1:].upper()) and(a[0]==a[0].lower())):
+    print (a[0].upper()+a[1:].lower())
+elif (a==a.upper()):
+    print (a.lower())
+else:
+    print (a)
