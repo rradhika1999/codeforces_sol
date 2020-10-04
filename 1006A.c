612f31b7b2fb208d7679ca3f0c60cd093405ebaa
rradhika1999 <39021888+rradhika1999@users.noreply.github.com>
2020-10-04 15:17:10 +0530

    Update 1006A.c
    Linked the question at the top as a comment.
    Made changes in the for loop.
    Printed the array using a for loop at the end.

--- 1006A.c
+++ 1006A.c
@@ -1,3 +1,5 @@
+/*https://codeforces.com/problemset/problem/1006/A*/
+
 #include <assert.h>
 #include <limits.h>
 #include <math.h>
@@ -8,19 +10,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main (){
-    int n;
+int main ()
+{
+    int i,n;
     scanf("%d",&n);
-    int i;
-    int a[n];
-    for(i=0;i<n;i++){
+    int a[n+10];
+    for(i=0;i<n;i++)
+    {
         scanf("%d",&a[i]);
-        if(a[i]%2==0){
-            printf("%d ",a[i]-1);
-        }
-        else {
-            printf("%d ",a[i]);
+        if(a[i]%2==0)
+        {
+            a[i]=a[i]-1;
         }
     }
-    
+    for(int i=0;i<n-1;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("%d\n",a[n-1]);
 }
